528bc4c8e701a97030e7baa37b2d604b279bde30
Tyler Peterson <petersontb@familysearch.org>
2014-03-24 16:25:16 -0600

    Parameterize the template.
    Instead of a hardcoded link in the view render the link based on
    variables.
    The hardcoded values are in the controller.  Fix that next.

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Scripture Lottery' });
+  res.render('index', { title: 'Scripture Lottery', work: 'bofm', book: '2-ne', chapter: '9', verse: '28' });
 };
