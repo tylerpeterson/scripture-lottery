b7b09c6b09d37d16ded922f48a959bea4b8f03f7
Tyler Peterson <petersontb@familysearch.org>
2014-03-28 10:40:35 -0600

    Use native json loading.
    Got rid of the yaml code I couldn’t get to work.
    Load the JSON using the native require.  Thanks @redbugz for the tip.

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,7 @@ var Q = require('q'),
     FS = require('fs'),
     path = require('path'),
     readFile = Q.denodeify(FS.readFile),
-    yaml = require('yaml'),
-    scriptures;
-
-readFile(path.join(__dirname, '..', 'scriptures.json'), 'utf-8').then(function (yamlMarkup) {
-  try {
-    scriptures = JSON.parse(yamlMarkup);
-    console.log('scriptures', scriptures);
-  } catch (err) {
-    console.log('err parsing yaml', err);
-  }
-}, function (err) {
-  console.log('err loading yaml:', err);
-});
+    scriptures = require('../scriptures');
 
 function randomIntegerLessThan(num) {
   return Math.floor(Math.random() * num);
