f92154f8613a033fc89e9acbd4f6f7b4544c1419
Tyler Peterson <petersontb@familysearch.org>
2014-03-29 09:37:03 -0600

    Start writing scraper for verse numbers
    Because I’m lazy write a scraper for the last verse in each chapter.
    Then I don’t have to type them all in manually.
    Using a streaming parser isn’t the easiest solution, but feels really
    cool. I may have to cry uncle eventually and just use a dom parser.

---
+++ devUtils/nephi-scraper.js
@@ -0,0 +1,47 @@
+var request = require('superagent'),
+    htmlparser = require('htmlparser2'),
+    parser;
+
+(function () {
+  var inPar = false,
+      versePattern = /^(\d+)\./,
+      lastVerse = 1;
+
+  parser = new htmlparser.Parser({
+    // TODO parse the 'next' link as well.
+    onopentag: function (tagname) {
+      if (tagname === 'p') {
+        inPar = true;
+      }
+    },
+    ontext: function (text) {
+      var matches;
+
+      if (inPar && text) {
+        matches = text.match(versePattern);
+        if (matches) {
+          lastVerse = matches[0];
+          console.log('found verse', lastVerse);
+        }
+      }
+    },
+    onclosetag: function (tagname) {
+      if (tagname === 'html') {
+        console.log('reached document end. last verse:', lastVerse);
+        parser.lastVerse = lastVerse;
+      }
+      if (tagname === 'p') {
+        inPar = false;
+      }
+    }
+  });
+})();
+
+request.get('http://scriptures.nephi.org/docbook/bom/c6.html')
+    .end(function (err, res) {
+      parser.write(res.text);
+      parser.end();
+      console.log('parser returned', parser.lastVerse);
+      // console.log(res.text);
+    });
+console.log('ran');
