3faf975fabb5cf731986c764ca899940c4489078
Tyler Peterson <petersontb@familysearch.org>
2014-04-02 11:47:44 -0600

    Unit test the verse number parsing
    Write unit tests for the functionality already implemented —
    preparatory to refactoring.

--- test/walker-test.js
+++ test/walker-test.js
@@ -1,12 +1,26 @@
 var scraper = require('../devUtils/nephi-scraper'),
-    expect = require('chai').expect;
+    expect = require('chai').expect,
+    fs = require('fs'),
+    path = require('path');
 
 console.log('in test');
 
 describe('Scraper', function () {
+  var html;
+
+  before(function () {
+    html = fs.readFileSync(path.join(__dirname, '1-ne_1.html'));
+  });
+
   it('should export the parser', function () {
     expect(scraper).to.be.defined;
     expect(scraper.write).to.be.a('function');
     expect(scraper.end).to.be.a('function');
+  });
+
+  it('should remember the last verse number', function () {
+    scraper.write(html);
+    scraper.end();
+    expect(scraper.lastVerse).to.equal('20');
   });
 });
