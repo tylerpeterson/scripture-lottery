7e9f06b0e753c8bdbd469a90a2fe16cca09b6811
Tyler Peterson <petersontb@familysearch.org>
2014-05-18 11:49:07 -0600

    Loop until all next links followed.
    Use recursive function to follow all next links.
    Throttle requests to be a little kinder.

--- devUtils/nephi-scraper.js
+++ devUtils/nephi-scraper.js
@@ -2,7 +2,11 @@ var request = require('superagent'),
     url = require('url'),
     parser = require('./nephi-parser'),
     docUrl = 'http://scriptures.nephi.org/docbook/bom/c6.html',
-    Q = require('q');
+    Q = require('q'),
+    debug = require('debug')('nephi-scraper'),
+    fs = require('fs'),
+    path = require('path');
+
 
   // TODO compute the next url based on the parsed nextlink and the base url
   // TODO make the walker know about url being parsed so nexurl is easier to compute
@@ -13,7 +17,11 @@ function process(docUrl) {
       .end(function (err, res) {
         var data = parser(res.text);
         console.log('parser returned book(%s) chapter(%s) lastverse(%s) nextlink(%s)', data.book, data.chapter, data.lastVerse, data.nextLink);
-        docUrl = url.resolve(docUrl, data.nextLink);
+        if (data.nextLink) {
+          docUrl = url.resolve(docUrl, data.nextLink);
+        } else {
+          docUrl = null;
+        }
         console.log('next document url (%s)', docUrl);
         // TODO translate long book names to short names
         dfd.resolve({book: data.book, chapter: data.chapter, lastVerse: data.lastVerse, nextLink: docUrl});
@@ -21,9 +29,30 @@ function process(docUrl) {
   return dfd.promise;
 }
 
+function processAll(accumulator, docUrl) {
+  return process(docUrl).delay(350).then(function (data) {
+    var next = data.nextLink;
+
+    accumulator[data.book] = accumulator[data.book] || {};
+    accumulator[data.book][data.chapter] = data.lastVerse;
+
+    fs.writeFileSync(path.join(__dirname, 'output/bom-verses.json'), JSON.stringify(accumulator, null, 2));
+
+    if (next) {
+      debug('recursing. Fetching %s', next);
+      return processAll(accumulator, next);
+    } else {
+      debug('Done!');
+      return accumulator;
+    }
+  });
+}
+
 if (require.main === module) {
-  process(docUrl).then(function (data) {
-    process(data.nextLink);
+  processAll({}, docUrl).then(function (bomVerses) {
+    debug('main done', bomVerses);
+  }, function (err) {
+    debug(err);
   });
   console.log('ran');
 }
