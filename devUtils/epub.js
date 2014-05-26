46f5a53b1db1f2256329d07dec3f9f3529e4cefc
Tyler Peterson <petersontb@familysearch.org>
2014-05-26 13:33:04 -0600

    start parsing epub data

--- devUtils/epub.js
+++ devUtils/epub.js
@@ -1,13 +1,25 @@
 var EPub = require('epub');
 var path = require('path');
 var epub = new EPub(path.join(__dirname, '../epubs/book-of-mormon-eng.epub'));
+var htmlparser = require('htmlparser2');
+var debug = require('debug')('scripture-lottery');
+var parser;
 
 epub.on("end", function(){
   // epub is now usable
   console.log(epub.metadata.title);
 
   epub.flow.forEach(function(chapter){
-      console.log(chapter.id);
+    var id = chapter.id;
+    var parts = id.split('_');
+    var book = parts[1];
+    var chapterNum = parseInt(parts[2]);
+
+    if (chapterNum > 0) {
+      debug("Found booK '%s' chapter %d", book, chapterNum);
+    } else {
+      debug('Chapter id "%s" isn\'t a normal chapter.', id);
+    }
   });
 
   epub.getChapter('lds_moro_010', function (err, text) {
