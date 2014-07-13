1c0e938fcbacebd5024c686c43a79a72a3ff8ed7
Tyler Peterson <petersontb@familysearch.org>
2014-07-13 12:38:12 -0600

    Allow limiting the work
    passing in the work should fix it to that work only
    passing the work is optional
    create a route for the Book of Mormon as first example

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/bofm', routes.bofm);
 app.get('/users', user.list);
 app.get('*', routes.e404);
 
--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,10 @@ function pickOne(list) {
   return list[randomIntegerLessThan(list.length)];
 }
 
-function generateViewParams() {
-  var work = pickOne(Object.keys(scriptures)),
-      book = pickOne(Object.keys(scriptures[work])),
+function generateViewParams(work) {
+  work = work || pickOne(Object.keys(scriptures));
+
+  var book = pickOne(Object.keys(scriptures[work])),
       chapter = Number(pickOne(Object.keys(scriptures[work][book]))) + 1,
       verse = randomIntegerLessThan(scriptures[work][book][chapter - 1]) + 1,
       anchor = '#' + (verse > 1 ? verse - 1 : verse),
@@ -30,11 +31,18 @@ function generateViewParams() {
 
 exports.index = function(req, res){
   var viewParams = generateViewParams();
-  
+
   debug(viewParams);
   viewParams.title = 'Scripture Lottery';
   res.render('index', viewParams);
 };
+
+exports.bofm = function (req, res) {
+  var viewParams = generateViewParams('bofm');
+
+  viewParams.title = 'Book of Mormon Scripture Lottery';
+  res.render('index', viewParams);  
+}
 
 exports.e404 = function (req, res) {
   // TODO try serving as static asset instead of rendered view
