125a6f35ba2f392122daa5c385ef9bbabe9084f8
Tyler Peterson <petersontb@familysearch.org>
2014-07-13 12:32:28 -0600

    extract function for picking the reading
    prepare for routes that limit readings to particular works

--- routes/index.js
+++ routes/index.js
@@ -13,19 +13,24 @@ function pickOne(list) {
   return list[randomIntegerLessThan(list.length)];
 }
 
-
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
+function generateViewParams() {
   var work = pickOne(Object.keys(scriptures)),
       book = pickOne(Object.keys(scriptures[work])),
       chapter = Number(pickOne(Object.keys(scriptures[work][book]))) + 1,
       verse = randomIntegerLessThan(scriptures[work][book][chapter - 1]) + 1,
       anchor = '#' + (verse > 1 ? verse - 1 : verse),
       viewParams = {work: work, book: book, chapter: chapter, verse: verse, anchor: anchor};
+  
+  return viewParams;  
+}
 
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  var viewParams = generateViewParams();
+  
   debug(viewParams);
   viewParams.title = 'Scripture Lottery';
   res.render('index', viewParams);
